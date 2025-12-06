49b23e4a8dcb247f1177a6113c13eed20363a799
Yovanaedith <yovanaedith2000@gmail.com>
2025-12-06 00:22:16 -0200

    fix: configurar base path para github pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Practica2-Portafolio/',
+  base: '/Practica2-Portafolio/', 
   plugins: [react()],
 })
